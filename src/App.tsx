ea5b2211be612e1455ba63fb425f0e3644f1a428
alextsapin <96056751+alextsapin@users.noreply.github.com>
2024-08-09 17:12:03 +0200

    Start project

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useState} from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { Error404 } from './components/pages/Error404';
+import { Adidas } from './components/pages/Adidas';
+import { PageThree } from './components/pages/Abibas';
+import { PageTwo } from './components/pages/Puma';
+import styles from "./components/Site.module.css";
+import { Model } from './components/pages/Model';
+
+function App() {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <NavLink to="/adidas" className={({isActive}) => isActive ? 'active' : ''}>Adidas</NavLink>
+                    <NavLink to="/puma">Puma</NavLink>
+                    <NavLink to="/abibas">Abibas</NavLink>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/adidas"/>}/>
+                        <Route path="/adidas" element={<Adidas/>}/>
+                        <Route path="/adidas/:id" element={<Model/>}/>
+                        <Route path="/puma" element={<PageTwo/>}/>
+                        <Route path="/abibas" element={<PageThree/>}/>
+                        <Route path="*" element={<Error404/>}/>
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
+        </div>
+    );
+}
+
+export default App;
